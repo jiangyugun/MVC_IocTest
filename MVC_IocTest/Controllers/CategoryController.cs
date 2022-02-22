c8b6e59b1cf59e91d4dd9dd543b2f9632a1d6b9c
jiangyugun <47242466+jiangyugun@users.noreply.github.com>
2022-02-22 23:31:32 +0800

    新增Category、Product兩個Controller以及相對應的View

---
+++ MVC_IocTest/Controllers/CategoryController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using MVC_IocTest.Models;
+
+namespace Mvc_Repository.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public ActionResult Index()
+        {
+            using (TestDBEntities db = new TestDBEntities())
+            {
+                var query = db.Categories.OrderBy(x => x.CategoryID);
+                ViewData.Model = query.ToList();
+                return View();
+            }
+        }
+
+        //=========================================================================================
+
+        public ActionResult Details(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return RedirectToAction("index");
+            }
+            else
+            {
+                using (TestDBEntities db = new TestDBEntities())
+                {
+                    var model = db.Categories.FirstOrDefault(x => x.CategoryID == id.Value);
+                    ViewData.Model = model;
+                    return View();
+                }
+            }
+        }
+
+        //=========================================================================================
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Categories category)
+        {
+            if (category != null && ModelState.IsValid)
+            {
+                using (TestDBEntities db = new TestDBEntities())
+                {
+                    db.Categories.Add(category);
+                    db.SaveChanges();
+                }
+                return RedirectToAction("index");
+            }
+            else
+            {
+                return View(category);
+            }
+        }
+
+        //=========================================================================================
+
+        public ActionResult Edit(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return RedirectToAction("index");
+            }
+            else
+            {
+                using (TestDBEntities db = new TestDBEntities())
+                {
+                    var model = db.Categories.FirstOrDefault(x => x.CategoryID == id.Value);
+                    ViewData.Model = model;
+                    return View();
+                }
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Categories category)
+        {
+            if (category != null && ModelState.IsValid)
+            {
+                using (TestDBEntities db = new TestDBEntities())
+                {
+                    db.Entry(category).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return View(category);
+                }
+            }
+            else
+            {
+                return RedirectToAction("index");
+            }
+        }
+
+        //=========================================================================================
+
+        public ActionResult Delete(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return RedirectToAction("index");
+            }
+            else
+            {
+                using (TestDBEntities db = new TestDBEntities())
+                {
+                    var model = db.Categories.FirstOrDefault(x => x.CategoryID == id.Value);
+                    ViewData.Model = model;
+                    return View();
+                }
+            }
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            try
+            {
+                using (TestDBEntities db = new TestDBEntities())
+                {
+                    var target = db.Categories.FirstOrDefault(x => x.CategoryID == id);
+                    db.Categories.Remove(target);
+                    db.SaveChanges();
+                }
+            }
+            catch (DataException)
+            {
+                return RedirectToAction("Delete", new { id = id });
+            }
+            return RedirectToAction("index");
+        }
+
+    }
+}
---
+++ MVC_IocTest/Controllers/ProductController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_IocTest.Models;
+
+namespace Mvc_Repository.Controllers
+{
+    public class ProductController : Controller
+    {
+        private TestDBEntities db = new TestDBEntities();
+
+        public ActionResult Index()
+        {
+            var products = db.Products.Include(p => p.Categories).OrderByDescending(x => x.ProductID);
+            return View(products.ToList());
+        }
+
+        //=========================================================================================
+
+        public ActionResult Details(int id = 0)
+        {
+            Products products = db.Products.Find(id);
+            if (products == null)
+            {
+                return HttpNotFound();
+            }
+            return View(products);
+        }
+
+        //=========================================================================================
+
+        public ActionResult Create()
+        {
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Products products)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(products);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", products.CategoryID);
+            return View(products);
+        }
+
+        //=========================================================================================
+
+        public ActionResult Edit(int id = 0)
+        {
+            Products products = db.Products.Find(id);
+            if (products == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", products.CategoryID);
+            return View(products);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Products products)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(products).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", products.CategoryID);
+            return View(products);
+        }
+
+        //=========================================================================================
+
+        public ActionResult Delete(int id = 0)
+        {
+            Products products = db.Products.Find(id);
+            if (products == null)
+            {
+                return HttpNotFound();
+            }
+            return View(products);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Products products = db.Products.Find(id);
+            db.Products.Remove(products);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //=========================================================================================
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
