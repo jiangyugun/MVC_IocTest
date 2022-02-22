8135c160e0eca66dc869e6cc36caa24c4b080b97
jiangyugun <47242466+jiangyugun@users.noreply.github.com>
2022-02-22 23:33:05 +0800

    建立 ICategoryRepository 與 IProductRepository

---
+++ MVC_IocTest/Models/Interface/ICategoryRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_IocTest.Models.Interface
+{
+    public interface ICategoryRepository
+    {
+        void Create(Categories instance);
+
+        void Update(Categories instance);
+
+        void Delete(Categories instance);
+
+        Categories Get(int categoryID);
+
+        IQueryable<Categories> GetAll();
+
+        void SaveChanges();
+    }
+}
---
+++ MVC_IocTest/Models/Interface/IProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_IocTest.Models.Interface
+{
+    public interface IProductRepository
+    {
+        void Create(Products instance);
+
+        void Update(Products instance);
+
+        void Delete(Products instance);
+
+        Products Get(int productID);
+
+        IQueryable<Products> GetAll();
+
+        void SaveChanges();
+
+    }
+}
